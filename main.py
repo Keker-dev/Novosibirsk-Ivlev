52c28dc1f91e6a655ceca319b13adc6e960d23d7
Ивлев Тимофей <timaiv112008@gmail.com>
2024-12-02 18:01:09 +0700

    Модифицирован main.py. Git и случайные окружности. Теперь окружности должны быть не только случайного размера, но и случайного цвета.

--- main.py
+++ main.py
@@ -2,29 +2,34 @@ import sys
 from random import randint as rd
 from PyQt6.QtCore import QPoint
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtWidgets import QMainWindow, QApplication
-from PyQt6 import uic
-import io
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
 
-with open("UI.ui", "r", encoding="utf8") as f:
-    TEMPLATE = f.read()
+
+class MainWind(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(300, 300, 800, 600)
+        self.setWindowTitle('Рисование')
+        self.CreateButton = QPushButton("Создать круг", self)
+        self.CreateButton.resize(221, 51)
+        self.CreateButton.move(290, 10)
 
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
-class Example(QMainWindow):
+class Example(MainWind):
     def __init__(self):
         super().__init__()
-        uic.loadUi(io.StringIO(TEMPLATE), self)
         self.CreateButton.clicked.connect(self.update)
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
+        color = QColor(rd(0, 255), rd(0, 255), rd(0, 255))
         size = rd(10, 150)
+        qp.setBrush(color)
         qp.drawEllipse(QPoint(400, 300), size, size)
         qp.end()
 
