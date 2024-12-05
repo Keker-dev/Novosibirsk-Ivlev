154a9d8c6f596dc475509ee61cc88c13911737b2
Ивлев Тимофей <timaiv112008@gmail.com>
2024-12-05 18:32:34 +0700

    Модифицирован main.py, coffee.sqlite, main.ui. Добавлен addEditCoffeeForm.ui. Капучино

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import sys
+import sqlite3
 from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget
 from PyQt6 import uic
-import io
-
-with open("main.ui", "r", encoding="utf8") as f:
-    TEMPLATE = f.read()
 
 
 def except_hook(cls, exception, traceback):
@@ -13,12 +10,17 @@ def except_hook(cls, exception, traceback):
 
 
 class Example(QMainWindow):
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
-        uic.loadUi(io.StringIO(TEMPLATE), self)
+        uic.loadUi("main.ui", self)
+        self.name = name
 
+        self.show_table()
+        self.addButton.clicked.connect(self.open_sec_form)
+
+    def show_table(self):
         db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('coffee.sqlite')
+        db.setDatabaseName(self.name)
         db.open()
 
         model = QSqlTableModel(self, db)
@@ -29,10 +31,42 @@ class Example(QMainWindow):
         self.view.move(10, 10)
         self.view.resize(780, 580)
 
+    def open_sec_form(self):
+        self.SecForm = SecondForm(self, self.show_table, 'coffee.sqlite')
+        self.SecForm.show()
+
+
+class SecondForm(QWidget):
+    def __init__(self, *args):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.name = args[-1]
+        self.OKButton.clicked.connect(self.add_value)
+        self.OKButton.clicked.connect(args[-2])
+        self.OKButton.clicked.connect(self.close)
+
+    def add_value(self):
+        con = sqlite3.connect(self.name)
+        cur = con.cursor()
+        mx_id = max(map(lambda a: a[0], cur.execute("SELECT id from Coffee").fetchall())) + 1
+        brand = self.VarEdit.text()
+        roast = self.RoastEdit.text()
+        beans = self.BeansEdit.text()
+        desc = self.DescEdit.text()
+        coast = self.CoastEdit.text()
+        mass = self.MassEdit.text()
+        if coast.isdigit() and mass.isdigit():
+            coast, mass = int(coast), int(mass)
+        else:
+            coast, mass = 0, 0
+        params = (mx_id, brand, roast, beans, desc, coast, mass)
+        cur.execute(f"""INSERT INTO Coffee(ID, varietie, roast, beans, describe, coast, mass) VALUES {params}""")
+        con.commit()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Example('coffee.sqlite')
     ex.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
